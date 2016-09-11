ea0f2a3b0555d7a4f71715e8bdcaba4407d5ebc2
nathan anderson <anderson@nathanworking.com>
2016-09-10 22:46:32 -0400

    New Photos page - all photos

--- js/photos.js
+++ js/photos.js
@@ -53,6 +53,13 @@ jQuery(document).ready(function($){
 	}
 });
 
+
+/// Lazy loading
+window.lazySizesConfig = window.lazySizesConfig || {};
+//page is optimized for fast onload event
+lazySizesConfig.loadMode = 1;
+
+
 gallery = $('#Container').lightGallery({
   selector: '.photo-link',
   download: false,
@@ -62,57 +69,10 @@ gallery = $('#Container').lightGallery({
   mode: 'lg-fade'
 });
 
-
-
-$(function(){
-
- var layout = 'list', // Store the current layout as a variable
-     $container = $('#Container'), // Cache the MixItUp container
-     $changeLayout = $('#ChangeLayout'); // Cache the changeLayout button
-
- // Instantiate MixItUp with some custom options:
-
- $container.mixItUp({
-   animation: {
-     duration: 400,
-     effects: 'fade stagger(34ms)',
-     easing: 'ease',
-     animateResizeContainer: false, // Needed to make flex-mansory layout work
-   }
- });
-
- // MixItUp does not provide a default "change layout" button, so we need to make our own and bind it with a click handler:
-
- $changeLayout.on('click', function(){
-
-   // If the current layout is a list, change to grid:
-
-   if(layout == 'list'){
-     layout = 'grid';
-
-     $changeLayout.text('List'); // Update the button text
-
-     $container.mixItUp('changeLayout', {
-       containerClass: layout // change the container class to "grid"
-     });
-
-   // Else if the current layout is a grid, change to list:
-
-   } else {
-     layout = 'list';
-
-     $changeLayout.text('Grid'); // Update the button text
-
-     $container.mixItUp('changeLayout', {
-       containerClass: layout // Change the container class to 'list'
-     });
-   }
- });
-
+$('#Container').isotope({
+  itemSelector: '.mix',
+  masonry: {
+    columnWidth: 50,
+    gutter: 10
+  }
 });
-
-
-/// Lazy loading
-window.lazySizesConfig = window.lazySizesConfig || {};
-//page is optimized for fast onload event
-lazySizesConfig.loadMode = 1;
