540e707bd4f5faa71526bb3f58def502a2816145
nathan anderson <anderson@nathanworking.com>
2016-09-10 22:47:07 -0400

    New Photos Page

---
+++ js/photos.js
@@ -0,0 +1,78 @@
+/*-----------------------------------
+
+    ----- Smart Navigation ------
+
+--------------------------------------*/
+jQuery(document).ready(function($){
+	// browser window scroll (in pixels) after which the "menu" link is shown
+	var offset = 300;
+
+	var navigationContainer = $('#cd-nav'),
+		mainNavigation = navigationContainer.find('#cd-main-nav ul');
+
+	//hide or show the "menu" link
+	checkMenu();
+	$(window).scroll(function(){
+		checkMenu();
+	});
+
+	//open or close the menu clicking on the bottom "menu" link
+	$('.cd-nav-trigger').on('click', function(){
+		$(this).toggleClass('menu-is-open');
+		//we need to remove the transitionEnd event handler (we add it when scolling up with the menu open)
+		mainNavigation.off('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend').toggleClass('is-visible');
+
+	});
+
+	function checkMenu() {
+		if( $(window).scrollTop() > offset && !navigationContainer.hasClass('is-fixed')) {
+			navigationContainer.addClass('is-fixed').find('.cd-nav-trigger').one('webkitAnimationEnd oanimationend msAnimationEnd animationend', function(){
+				mainNavigation.addClass('has-transitions');
+			});
+		} else if ($(window).scrollTop() <= offset) {
+			//check if the menu is open when scrolling up
+			if( mainNavigation.hasClass('is-visible')  && !$('html').hasClass('no-csstransitions') ) {
+				//close the menu with animation
+				mainNavigation.addClass('is-hidden').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function(){
+					//wait for the menu to be closed and do the rest
+					mainNavigation.removeClass('is-visible is-hidden has-transitions');
+					navigationContainer.removeClass('is-fixed');
+					$('.cd-nav-trigger').removeClass('menu-is-open');
+				});
+			//check if the menu is open when scrolling up - fallback if transitions are not supported
+			} else if( mainNavigation.hasClass('is-visible')  && $('html').hasClass('no-csstransitions') ) {
+					mainNavigation.removeClass('is-visible has-transitions');
+					navigationContainer.removeClass('is-fixed');
+					$('.cd-nav-trigger').removeClass('menu-is-open');
+			//scrolling up with menu closed
+			} else {
+				navigationContainer.removeClass('is-fixed');
+				mainNavigation.removeClass('has-transitions');
+			}
+		}
+	}
+});
+
+
+/// Lazy loading
+window.lazySizesConfig = window.lazySizesConfig || {};
+//page is optimized for fast onload event
+lazySizesConfig.loadMode = 1;
+
+
+gallery = $('#Container').lightGallery({
+  selector: '.photo-link',
+  download: false,
+  counter: false,
+  zoom: false,
+  thumbnail: false,
+  mode: 'lg-fade'
+});
+
+$('#Container').isotope({
+  itemSelector: '.mix',
+  masonry: {
+    columnWidth: 50,
+    gutter: 10
+  }
+});
